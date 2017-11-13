6bb49012dbf2afd7487add920855c7e210007879
jakbay <j.akbay@free.fr>
2017-11-13 14:23:28 -0300

    Complete the first "game"

--- sfml/002_first_game/Game.cpp
+++ sfml/002_first_game/Game.cpp
@@ -1,17 +1,35 @@
 #include "Game.h"
 
-Game::Game() : mWindow(sf::VideoMode(640, 480), "SFML Application"), mPlayer()
+Game::Game() :
+mWindow(sf::VideoMode(1980, 720), "SFML Application"),
+mPlayer(),
+mIsMovingUp(false),
+mIsMovingDown(false),
+mIsMovingLeft(false),
+mIsMovingRight(false)
 {
-	mPlayer.setRadius(40.f);
+	mWindow.setVerticalSyncEnabled(true);
+	if (!mTexture.loadFromFile("resources/images/vulture.png"))
+	{
+		return;
+	}
+	mPlayer.setTexture(mTexture);
 	mPlayer.setPosition(100.f, 100.f);
-	mPlayer.setFillColor(sf::Color::Cyan);
 }
 
 void Game::run()
 {
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
 	while (mWindow.isOpen()) {
 		processEvents();
-		update();
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > TimePerFrame)
+		{
+			timeSinceLastUpdate -= TimePerFrame;
+			processEvents();
+			update(TimePerFrame);
+		}
 		render();
 	}
 }
@@ -21,14 +39,38 @@ void Game::processEvents()
 	sf::Event event;
 	while (mWindow.pollEvent(event))
 	{
-		if (event.type == sf::Event::Closed)
+		switch (event.type) {
+		case sf::Event::KeyPressed:
+			handlePlayerInput(event.key.code, true);
+			break;
+		case sf::Event::KeyReleased:
+			handlePlayerInput(event.key.code, false);
+			break;
+		case sf::Event::Closed:
 			mWindow.close();
+			break;
+		default:
+			break;
+		}
 	}
 }
 
-void Game::update()
+void Game::update(const sf::Time &deltaTime)
 {
-
+	sf::Vector2f movement(0.f, 0.f);
+	if (mIsMovingUp) {
+		movement.y -= PlayerSpeed;
+	}
+	if (mIsMovingDown) {
+		movement.y += PlayerSpeed;
+	}
+	if (mIsMovingLeft) {
+		movement.x -= PlayerSpeed;
+	}
+	if (mIsMovingRight) {
+		movement.x += PlayerSpeed;
+	}
+	mPlayer.move(movement * deltaTime.asSeconds());
 }
 
 void Game::render()
@@ -38,3 +80,22 @@ void Game::render()
 	mWindow.display();
 }
 
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	switch (key) {
+	case sf::Keyboard::Z:
+		mIsMovingUp = isPressed;
+		break;
+	case sf::Keyboard::S:
+		mIsMovingDown = isPressed;
+		break;
+	case sf::Keyboard::Q:
+		mIsMovingLeft = isPressed;
+		break;
+	case sf::Keyboard::D:
+		mIsMovingRight = isPressed;
+		break;
+	default:
+		break;
+	}
+}
